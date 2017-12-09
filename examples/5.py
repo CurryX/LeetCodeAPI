df2c421ff80fcb7d2b15a505476694e1d7d56cf8
Peng Wang <pengwang@cmu.edu>
2017-12-08 23:45:04 -0500

    Add more examples

---
+++ examples/5.py
@@ -0,0 +1,17 @@
+class Solution:
+    # @return a string
+    def longestPalindrome(self, s):
+        if len(s)==0:
+        	return 0
+        maxLen=1
+        start=0
+        for i in range(len(s)):
+        	if i-maxLen >=1 and s[i-maxLen-1:i+1]==s[i-maxLen-1:i+1][::-1]:
+        		start=i-maxLen-1
+        		maxLen+=2
+        		continue
+
+        	if i-maxLen >=0 and s[i-maxLen:i+1]==s[i-maxLen:i+1][::-1]:
+        		start=i-maxLen
+        		maxLen+=1
+        return s[start:start+maxLen]
